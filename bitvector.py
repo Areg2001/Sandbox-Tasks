d4f04f8598b9b74a8e6faae64bba5948cc5b0bf7
Areg2001 <107401750+Areg2001@users.noreply.github.com>
2023-02-02 17:39:22 +0400

    Add files via upload

--- bitvector.py
+++ bitvector.py
@@ -1,39 +1,24 @@
 import numpy as np
-import math
-from numpy import ndarray
 
 
-class Bitvector:
+class BitVector:
 
     def __init__(self, vector_size: int) -> None:
-        self.__array_index = None
-        self.__vector_size = vector_size
-        self.__array_size = math.ceil(vector_size / 64)
-        self.__array = np.zeros(self.__array_size, dtype="int64")
+        self.__array_size = (vector_size + 63) // 64
+        self.__array = np.zeros(self.__array_size, dtype="uint64")
 
     @property
-    def array(self) -> ndarray:
+    def array(self) -> np.ndarray:
         return self.__array
 
     @property
     def array_size(self) -> int:
         return self.__array_size
 
-    @property
-    def vector_size(self) -> int:
-        return self.__vector_size
-
     def set(self, index: int) -> None:
-        self.__array_index = index // 64
-        self.__array[self.__array_index] = (self.__array[self.__array_index]) ^ (1 << ((index % 64) + 29)
+        array_index = index // 64
+        self.__array[self.__array.size - array_index - 1] |= np.uint64(1 << index % 64)
 
     def reset(self, index: int) -> None:
-        self.__array_index = index // 64
-        self.__array[self.__array_index] = (self.__array[self.__array_index]) ^ (1 << (index % 64))
-
-
-x = Bitvector(100)
-print(x.array)
-x.set(98)
-print(x.array)
-print(int(32*"0" + "1" + 31*"0", 2))
+        array_index = index // 64
+        self.__array[self.__array.size - array_index - 1] &= np.uint64(0 << index % 64)
