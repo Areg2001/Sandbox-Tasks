473c40760030015fc32284c8c707918fae9006e0
Areg Melqonyan <aregmelqonyan@Aregs-MacBook-Pro.local>
2023-11-21 15:36:50 +0400

    Updated Bitvector

--- bitvector.py
+++ bitvector.py
@@ -4,21 +4,13 @@ import numpy as np
 class BitVector:
 
     def __init__(self, vector_size: int) -> None:
-        self.__array_size = (vector_size + 63) // 64
-        self.__array = np.zeros(self.__array_size, dtype="uint64")
-
-    @property
-    def array(self) -> np.ndarray:
-        return self.__array
-
-    @property
-    def array_size(self) -> int:
-        return self.__array_size
+        self.array_size = (vector_size // 64) + 1
+        self.array = np.zeros(self.array_size, dtype="uint64")
 
     def set(self, index: int) -> None:
         array_index = index // 64
-        self.__array[self.__array.size - array_index - 1] |= np.uint64(1 << index % 64)
+        self.array[self.array.size - array_index - 1] |= np.uint64(1 << index % 64)
 
     def reset(self, index: int) -> None:
         array_index = index // 64
-        self.__array[self.__array.size - array_index - 1] ^= np.uint64(1 << index % 64)
+        self.array[self.array.size - array_index - 1] &= ~np.uint64(1 << index % 64)
